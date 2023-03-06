09e2c73fec707b449fa3b241d62a6b3e299fada7
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-03-06 20:12:41 +0800

    tests: Add test for device

---
+++ tests/test_device.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from yeet_api.devices import Device, _DeviceJSON
+
+
+class DeviceTest(TestCase):
+    def setUp(self):
+        self.device = Device("testbrand", "testmodel")
+
+    def test_device(self):
+        self.assertEqual(self.device.full_name, "Test Device")
+        self.assertEqual(self.device.codename, "TESTDEV123")
+
+    def test_resources(self):
+        self.assertEqual(
+            self.device.get_available_resources(),
+            ("recovery", "kernel", "extraresource1", "extraresource2"),
+        )
+        self.assertEqual(
+            self.device.get_resource("recovery"),
+            {
+                "PBRP": "https://google.com",
+                "TWRP": "https://google.com",
+                "OFOX": "https://google.com",
+            },
+        )
