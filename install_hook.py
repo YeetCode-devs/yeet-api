d13845827bbcd5a84e6b10e57154b5358f74a1a8
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-03-09 15:57:24 +0800

    hook: Re-add files after formatting

--- install_hook.py
+++ install_hook.py
@@ -2,9 +2,18 @@
 
 from pathlib import Path
 
+SCRIPT = r"""#!/bin/sh
+set -o noglob
+
+python -m black .
+
+for file in $(git diff --cached --name-only); do
+    git add $file
+done"""
+
 
 def main() -> None:
     print("Installing pre-commit hook")
     with open(".git/hooks/pre-commit", "w") as f:
-        f.write("#!/bin/sh\npython -m black .")
+        f.write(SCRIPT)
     Path(".git/hooks/pre-commit").chmod(0o700)
