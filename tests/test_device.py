889fca565185e3eb42790e4740b3b0784996fe75
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-07-03 00:18:13 +0800

    tests: Adapt to new test device and cleanup test cases

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,26 +1,28 @@
 # SPDX-License-Identifier: MIT
 
 from unittest import TestCase
-from yeet_api.device import Device, _DeviceJSON
+from yeet_api.device import Device
 
 
 class DeviceTest(TestCase):
     def setUp(self):
-        self.device = Device("testbrand", "testmodel")
+        self.device = Device("testbrand_1", "testmodel_1_1")
 
     def test_attr(self):
-        self.assertEqual(self.device.brand, "testbrand")
-        self.assertEqual(self.device.model, "testmodel")
+        self.assertEqual(self.device.brand, "testbrand_1")
+        self.assertEqual(self.device.model, "testmodel_1_1")
 
     def test_device_metadata(self):
         self.assertEqual(self.device.fullname, "Test Device")
         self.assertEqual(self.device.codename, "TESTDEV123")
 
-    def test_resources(self):
+    def test_verify_resources(self):
         self.assertEqual(
             self.device.get_available_resources(),
             ("recovery", "kernel", "extraresource1", "extraresource2"),
         )
+
+    def test_resources(self):
         self.assertEqual(
             self.device.get_resource("recovery"),
             {
@@ -28,4 +30,22 @@ class DeviceTest(TestCase):
                 "TWRP": "https://google.com",
                 "OFOX": "https://google.com",
             },
+        )
+        self.assertEqual(
+            self.device.get_resource("kernel"),
+            {
+                "BBKERNEL": "https://google.com",
+                "PERFKERNEL": "https://google.com",
+                "BALANCEDKERNEL": "https://google.com",
+            },
+        )
+
+    def test_extra_resources(self):
+        self.assertEqual(
+            self.device.get_resource("extraresource1"),
+            {"UNLOCKSCRIPT": "https://google.com"},
+        )
+        self.assertEqual(
+            self.device.get_resource("extraresource2"),
+            {"BOOTLOADERUNLOCKGUIDE": "https://google.com"},
         )
